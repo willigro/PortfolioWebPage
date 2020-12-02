35c903c8161aebcbe93f4cf1d1ec60ce91f3e095
Rittmann <willirittmann@outlook.com>
2020-12-01 22:23:11 -0300

    Add color

--- home.php
+++ home.php
@@ -173,7 +173,7 @@
         <div class="content-title-section">
           <p class="p-title-section">My Skills</p>
           <p id="train-my-dino" class="p-content">
-            Train the blocks <a id="train-my-dino-link" class="a-main">clicking here</a>
+            Train the Dino Blocks <a id="train-my-dino-link" class="a-main">clicking here</a>
           </p>
           <div id="stop-train-my-dino" class="p-content" hidden>
             Stop the training <a id="stop-train-my-dino-link" class="a-main">clicking here</a>
@@ -182,7 +182,7 @@
               Wait, I'm learning!!
               <br>Score <span id="dino_actual_score">0</span>
               <br>Alive blocks <span id="dino_alive">0</span>
-              <br>Best score <span id="dino_best_score">0</span>
+              <br>Best score <span id="dino_best_score" class="p-shine">0</span>
             </div>
           </div>
 
